68d1f3b70343bc19ceaf32e45f359647f0729e60
Daniel Park <d.hanul.park@gmail.com>
2016-07-11 22:45:06 -0400

    add color picker code

--- imports/ui/draw.js
+++ imports/ui/draw.js
@@ -13,8 +13,13 @@ const sketch = require('sketch-js');
 import './draw.html';
 
 // Global Variables
+// Pen Color
 var lineColor = '#FFFFFF';
 Session.set('penColor', lineColor);
+// Color History
+var colorHistory = ['#333333']; 
+Session.set('colorHistory', colorHistory);
+// Pen Radius
 var currentRadius = 20;
 Session.set('penRadius', currentRadius);
 
@@ -34,24 +39,41 @@ function cloneCanvas(oldCanvas) {
 
 // Template Events
 Template.draw_canvas.events({
+  // Submitting a New Color
   'submit .pickColor'(event) {
     event.preventDefault(); 
 
     const color = event.target.color2.value; 
     lineColor = '#' + String(color); 
+    colorHistory.push('#' + String(color)); 
     Session.set('penColor', lineColor);
+    Session.set('colorHistory', colorHistory);
+    Session.set('colorChange', false);
   },
 
+  'change #color_value'(e) {
+    Session.set('colorChange', true);
+  },
+
+  // Decrease Pen Radius
   'click .decreaseR'(e) {
     e.preventDefault(); 
     currentRadius -= 5;
     Session.set('penRadius', currentRadius);
   },
 
+  // Increase Pen Radius
   'click .increaseR'(e) {
     e.preventDefault(); 
     currentRadius += 5; 
     Session.set('penRadius', currentRadius);
+  },
+
+  // Change Pen Color From History Colors
+  'click .pickFromHistory'(e) {
+    e.preventDefault(); 
+    var color = e.target.getAttribute('data-color');
+    Session.set('penColor', color);
   }
 });
 
@@ -60,15 +82,25 @@ Template.draw_canvas.helpers({
   penRadius() {
     return Session.get('penRadius');
   },
+
   penColor() {
     return Session.get('penColor');
+  },
+
+  colors() {
+    return Session.get('colorHistory');
+  },
+
+  colorChange() {
+    return Session.get('colorChange');
   }
 })
 
 // Template On Rendered 
 Template.draw_canvas.onRendered( function() {
+  // Canvas/Sketch plugin Code
   Sketch.create({
-      container: document.getElementById( 'container' ),
+      container: document.getElementById( 'draw-area' ),
       autoclear: false,
       fullscreen: false,
       width: 400, 
@@ -102,7 +134,7 @@ Template.draw_canvas.onRendered( function() {
             touch = this.touches[i];
             this.lineCap = 'round';
             this.lineJoin = 'round';
-            this.fillStyle = this.strokeStyle = lineColor;
+            this.fillStyle = this.strokeStyle = Session.get('penColor');
             this.lineWidth = currentRadius;
             this.beginPath();
             this.moveTo( touch.ox, touch.oy );
@@ -111,7 +143,8 @@ Template.draw_canvas.onRendered( function() {
           }
         }
 
-        var oldCanvas = document.getElementById( 'container' ).getElementsByTagName( 'canvas' )[0];
+        // In order to clone the canvas onto many walls.
+        var oldCanvas = document.getElementById( 'draw-area' ).getElementsByTagName( 'canvas' )[0];
         cloneCanvas(oldCanvas);
       }
   });
